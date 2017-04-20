a62ea8963f870c638aa8c2856ef961d154564409
seliver <alexey.seliverstov.dev@gmail.com>
2017-04-20 15:20:09 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,42 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install zsh zsh-completions
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew install wget
+brew install pianobar
+brew install vim
+brew install rpl
+brew install node
+brew install svn
+brew install git
+brew install dnsmasq
+brew install cask
+brew cask install virtualbox
+brew cask install rest
+brew cask install vagrant
+brew cask install google-chrome
+brew cask install phpstorm
+brew cask install iterm2
+brew cask install slack
+brew cask install sequel-pro
+brew cask install keepassx
+brew cask install macpass
+brew cask install android-studio
+brew cask install torbrowser
+brew cask install qbittorrent
+brew cask install spotify
+brew cask install flux
+brew cask install evernote
+brew cask install sitesucker
+brew cask install mysqlworkbench
+brew cask install firefox
+brew cask install sublime-text
+brew cask install google-drive
+brew cask install skitch
+brew cask install spectacle
+brew cask install logitech-control-center
+npm install -g imgcat-cli
+brew cask cleanup
+brew cleanup
+brew doctor
+defaults write com.apple.finder AppleShowAllFiles TRUE
+killall Finder
